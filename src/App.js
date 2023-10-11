4eeb0b41f31beca524ed83c68ccc0097e2a2544e
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-10-11 03:31:28 +0200

    Importing login paget to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import LoginPage from "./pages/LoginPage";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
