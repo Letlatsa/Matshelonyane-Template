e707c045c914d7f09f7148f0462d6871e696b4d0
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-10-13 02:13:34 +0200

    Optimzed stuling

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -26,26 +26,50 @@ const LoginForm = () => {
     setAccountType(event.target.value);
   };
 
+  const styledFormControl = {
+    width: '100%',
+    color: 'white'
+  };
+
+  const styledTypography = {
+    textShadow: '4px 4px 6px rgba(0, 0, 0, 0.3)'
+  };
+
+  const styledTextField = {
+    width: '100%',
+    '& input': {
+      color: 'white',
+      borderBottom: ' 3px solid white'
+    },
+    '& label': {
+      color: 'white'
+    },
+    marginBottom: '10px'
+  };
+
+  const styledInputLabel = {
+    marginTop: 4,
+    left: '-2%',
+    color: 'white',
+    '&:hover': {
+      color: 'white'
+    }
+  };
+
+  const styledSelect = {
+    width: '100%',
+    color: 'white',
+    borderBottom: ' 2px solid white'
+  };
+
   return (
-    <Box sx={{}}>
-      <FormControl sx={{ width: '100%', color: 'white' }}>
+    <Box>
+      <FormControl sx={styledFormControl}>
         <Box sx={{ right: '10px !important' }}>
-          <Typography sx={{ textShadow: '4px 4px 6px rgba(0, 0, 0, 0.3)' }}>
-            Welcome to Matshelonyane!
-          </Typography>
+          <Typography sx={styledTypography}>Welcome to Matshelonyane!</Typography>
         </Box>
         <Box sx={{ width: '100%', marginTop: '20px' }}>
-          <InputLabel
-            id="Account-type"
-            sx={{
-              marginTop: 4,
-              left: '-2%',
-              color: 'white',
-              '&:hover': {
-                color: 'white'
-              }
-            }}
-          >
+          <InputLabel id="Account-type" sx={styledInputLabel}>
             Account type
           </InputLabel>
           <Select
@@ -54,10 +78,7 @@ const LoginForm = () => {
             id="cars"
             value={accountType}
             onChange={handleChange}
-            sx={{
-              width: '100%',
-              color: 'white'
-            }}
+            sx={styledSelect}
           >
             <MenuItem value="Client">Client</MenuItem>
             <MenuItem value="Driver">Driver</MenuItem>
@@ -68,19 +89,7 @@ const LoginForm = () => {
             type="phone"
             name="phone"
             placeholder="Enter your phone number"
-            sx={{
-              width: '100%',
-              '& input': {
-                color: 'white',
-                borderBottom: ' 3px solid white'
-              },
-              '& label': {
-                color: 'white'
-              },
-              '& input:hover': {
-                borderBottom: '1px solid white'
-              }
-            }}
+            sx={styledTextField}
           />
           <TextField
             variant="standard"
@@ -88,17 +97,7 @@ const LoginForm = () => {
             type="password"
             name="password"
             placeholder="Enter your password"
-            sx={{
-              width: '100%',
-
-              '& input': {
-                color: 'white',
-                borderBottom: ' 3px solid white'
-              },
-              '& label': {
-                color: 'white'
-              }
-            }}
+            sx={styledTextField}
           />
         </Box>
         <Box sx={{ width: '100%', marginTop: '50px' }}>
@@ -116,12 +115,13 @@ const LoginForm = () => {
           </Button>
           <LoginButton />
           <Typography
-            sx={{
-              color: 'white',
-              textAlign: 'center',
-              marginTop: '25px',
-              textShadow: '4px 4px 6px rgba(0, 0, 0, 0.3)'
-            }}
+            sx={
+              (styledTypography,
+              {
+                textAlign: 'center',
+                marginTop: '25px'
+              })
+            }
           >
             Don't have an account?
           </Typography>
