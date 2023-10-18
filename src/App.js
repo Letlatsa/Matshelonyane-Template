c0198413cdb1170027b4cc7d722788286bcb39e9
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-10-18 14:28:39 +0200

    Update App.js
    Creating routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { Route, Routes } from 'react-router-dom';
 import OneTimePin from './pages/OneTimePin';
 import Dashboard from './pages/Dashboard';
 
+/*New imports */
+import ClientHome from './pages/Home-pages/ClientHome';
+import ClientProfile from './pages/Home-pages/ClientProfile';
+import ClientTruckerProfile from './pages/Home-pages/ClientTruckerProfile';
+import Truckerhome from './pages/Home-pages/TruckerHome';
+import TruckerProfileView from './pages/Home-pages/TruckerProfileView';
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +21,13 @@ function App() {
         <Route path="/createaccount" element={<CreateAccount />}></Route>
         <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
         <Route path="/onetimepin" element={<OneTimePin />}></Route>
+
+        {/*New routes*/}
+        <Route path="/clienthome" element={<ClientHome />}></Route>
+        <Route path="/clientprofile" element={<ClientProfile />}></Route>
+        <Route path="/clienttruckerprofile" element={<ClientTruckerProfile />}></Route>
+        <Route path="/truckerhome" element={<Truckerhome />}></Route>
+        <Route path="/truckerprofileview" element={<TruckerProfileView />}></Route>
       </Routes>
     </div>
   );
