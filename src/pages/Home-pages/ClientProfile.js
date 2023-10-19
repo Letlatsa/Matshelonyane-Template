b0239b98e056f0c851bd90d3683f4a682bb79230
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-10-20 01:49:37 +0200

    Profile components

--- src/pages/Home-pages/ClientProfile.js
+++ src/pages/Home-pages/ClientProfile.js
@@ -1,7 +1,126 @@
+import { AppBar, Box, Button, Card, Container, IconButton, Toolbar, Typography } from '@mui/material';
+
+import BackArrow from '../../assets/backVector.svg';
+
 const ClientProfile = () => {
+  const styledProfileBox = {
+    borderRadius: '100px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EBDBD5',
+    padding: 0,
+    height: '100px',
+    width: '100px',
+    boxShadow: '4px 4px 6px rgba(0, 0, 0, 0.3)',
+    marginBottom: '15px'
+  };
+
+  const styledAppBar = {
+    backgroundColor: '#ffffff',
+    color: '#000000',
+    boxShadow: 'none'
+  };
+
   return (
     <div>
-      <h1>Client Profile</h1>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed" sx={styledAppBar}>
+          <Toolbar sx={{ height: '70px' }}>
+            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <img src={BackArrow} alt="MenuIcon" width="13" height="30" />
+            </IconButton>
+            <Typography
+              variant="h6"
+              sx={{
+                flexGrow: 1,
+                textAlign: 'end',
+                height: '50px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: '#58362A',
+                fontSize: '32px',
+                paddingTop: '5px'
+              }}
+            >
+              Profile
+            </Typography>
+            <Box
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2, width: '30px' }}
+            >
+              <Box></Box>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Container sx={{ marginTop: '90px' }}>
+        <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '50px' }}>
+          <Box sx={styledProfileBox}>
+            <img
+              src="https://picsum.photos/200/300"
+              alt=""
+              style={{ width: '95px', height: '95px', borderRadius: 100, backgroundColor: 'grey' }}
+            />
+          </Box>
+          <Typography
+            sx={{
+              color: ' #58362A',
+              fontFamily: 'Lato',
+              fontSize: '24px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              lineHeight: 'normal',
+              letterSpacing: '-0.17px'
+            }}
+          >
+            John Doe
+          </Typography>
+          <Typography
+            sx={{
+              color: '#C69585',
+              fontFamily: 'Lato',
+              fontSize: '16px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              lineHeight: 'normal',
+              letterSpacing: '-0.17px',
+              marginBottom: '15px'
+            }}
+          >
+            Gaborone
+          </Typography>
+          <Button
+            variant="text"
+            sx={{
+              backgroundColor: '#EBDBD5',
+              textColor: '#58362A',
+              width: '196px',
+              borderRadius: '15px',
+              height: '50px',
+              color: '#58362A',
+              fontWeight: '300',
+              fontSize: '14px',
+              textTransform: 'none',
+              boxShadow: '4px 4px 6px rgba(0, 0, 0, 0.3)',
+              '&:hover': {
+                backgroundColor: '#58362A',
+                color: 'white',
+                transition: 'ease-in .3s'
+              }
+            }}
+          >
+            Request Pickup
+          </Button>
+        </Box>
+        <Card>
+        </Card>
+      </Container>
     </div>
   );
 };
