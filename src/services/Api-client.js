dd561e6953e698eb0b07ae2d7966c0a58451805a
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-11-02 09:29:08 +0200

    Upated api client

--- src/services/Api-client.js
+++ src/services/Api-client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const BaseUrl = 'http://13.244.157.212/api/iam/v1';
+const BaseUrl = 'https://matshelonyane.com/api/iam/v1/';
 
 export default axios.create({ baseURL: BaseUrl });
