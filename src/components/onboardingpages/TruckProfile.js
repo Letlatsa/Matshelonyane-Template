dc57d3dacc5b33d6cf6bb42416af9747a2148bc2
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-11-08 14:44:03 +0200

    Updating state & styling

--- src/components/onboardingpages/TruckProfile.js
+++ src/components/onboardingpages/TruckProfile.js
@@ -101,7 +101,7 @@ function TruckProfile() {
   const [formData, setFormData] = useState(initialFormState);
   const [formErrors, setFormErrors] = useState(initialErrorState);
   const [avatarImage, setAvatarImage] = useState(null);
-  const [currentStep, setCurrentStep] = useState(1);
+  const [currentStep, setCurrentStep] = useState(3);
 
   const [file, setFile] = useState(null);
   const navigate = useNavigate();
