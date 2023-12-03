cc21bf3d2e5405d68c3a5597f6d193dc94c7e5ad
MAITUMELO RADIPODI <mradipodi@gmail.com>
2023-12-03 10:47:33 +0200

    added homeappbar to trucker component

--- src/components/HomeComponents/Trucker/JobPosting.js
+++ src/components/HomeComponents/Trucker/JobPosting.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import BottomNavigationComponent from './BottomNavigationComponent';
+import TruckerHomeAppBarComponents from './TruckerHomeAppBarComponent';
 
 function JobPosting() {
   return (
     <Box>
+      <TruckerHomeAppBarComponents />
       <BottomNavigationComponent />
     </Box>
   );
